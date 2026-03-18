5e0aa2181200168f29cba32bb9b61996df9cbaa9
ayushkumar0258 <ayushkumar0258@gmail.com>
2026-03-18 22:22:34 +0200

    New Practice session changes

--- Coding/Python/Practice/Python_DataType_Practice.py
+++ Coding/Python/Practice/Python_DataType_Practice.py
@@ -1,1 +1,20 @@
-sds
+dict={"id":"300","name":"Arushi","gender":"Male","mob":"7895008172"}
+print("Printing the gender of the directory   :   ",dict["gender"])
+dict["rollNo"]=1366577
+print("newly added key value : ",dict)
+dict.pop("name")
+print("Directory after removing key name  : ", dict)
+#########################################
+tuple_data=(2,4,5,6,3,2,3,5,6,8)
+print("current tuple date    :  ",tuple_data)
+print("Value at index 3 are following : ",tuple_data[3])
+tuple_newData=("ayush",67.78,True,"Hi",1,2,3)
+print("Data of new tuple are follwoing : ",tuple_newData)
+
+
+
+
+
+
+
+
