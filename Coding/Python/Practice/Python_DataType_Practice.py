adbd12c3b502f39f89904c495f58b8d3c43488c6
ayushkumar0258 <ayushkumar0258@gmail.com>
2026-03-19 23:19:27 +0200

    After some practice on python

--- Coding/Python/Practice/Python_DataType_Practice.py
+++ Coding/Python/Practice/Python_DataType_Practice.py
@@ -22,4 +22,37 @@ print("set value after adding 0 in set : ",set_data)
 set_data.add(5)
 print("Adding 5 into set, 5 value already exists : ",set_data)
 
-
+print("module is :", 45%5)
+print("dividing number :",57/7)
+print("Exponational are following : ",9**5)
+print("Floor division give us Quotient making round to below  : ",47//8 )
+########################################
+my_data=5
+my_data=9
+my_data="Ayush Kumar"
+print("Till here value in my_data is : ",my_data)
+my_data=True
+my_data=67.89
+print("Value are following : ",my_data)
+my_data=["Hi","Ji","Kumar",78,45,True]
+print("We can set any collection on it : ",my_data)
+my_data=(4,3,5,4,6,7,23.90)
+print("New data tuple assign to same data_type : ", my_data)
+print("Data type till here should be tuple : ",type(my_data))
+my_data={4,5,4,3,4,6,7,6,8,9,"Hi","Ji","Hi"}
+print("new set assign to same date type :",my_data)
+print("Currently data type are ", type(my_data))
+################################## String slicing Practice ###########
+name="Ayush Kumar0258"
+slice1=name[:3]
+print("The String slice1 is :  ",slice1)
+slice2=name[2:8]
+print("The string slice2 is : ",slice2)
+slice3=name[::]
+print("the string slice3 is : ", slice3)
+slice4=name[2:9:3]
+print("the string slice4 is : ",slice4)
+slice5=name[-9:-3]
+print("the string slice5 is : ",slice5)
+slice6=name[::-2]
+print("the string slice6 is : ", slice6)
