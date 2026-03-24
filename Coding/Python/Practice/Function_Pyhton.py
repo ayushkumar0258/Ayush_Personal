ef881bb71d9f24816631e4bf8b49d36666172635
ayushkumar0258 <ayushkumar0258@gmail.com>
2026-03-24 22:15:43 +0200

    New changes for python function

---
+++ Coding/Python/Practice/Function_Pyhton.py
@@ -0,0 +1,38 @@
+num=234
+print("the type of this data",type(num))
+str_num=str(num)
+print("New string data type of num", type(str_num))
+str_string="My Name is AYush"
+print("Indexing values of this string",str_string[0])
+print("Indexing values of this string",str_string[0])
+print("the type of this data",type(num))
+str_num=str(num)
+print("New string data type of num", type(str_num))
+str_string="My Name is AYush"
+print("Indexing values of this string",str_string[0])
+print("Indexing values of this string",str_string[0])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
